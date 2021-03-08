fb3d9455ab333ae6dcf7684fc295205229c353a0
Thomas <tommyprezioso@gmail.com>
2021-03-08 13:30:35 -0500

    Initial Commit

---
+++ TodayIs/TodayIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayIsApp.swift
+//  TodayIs
+//
+//  Created by Thomas Prezioso Jr on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TodayIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

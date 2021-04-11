3c2cd32b7d0e951e45ad3fa43727f35c5c858414
Thomas <tommyprezioso@gmail.com>
2021-04-11 10:30:34 -0400

    working on getting different holidays titles and links

--- TodayIs/Utilities/Managers/NetworkManager.swift
+++ TodayIs/Utilities/Managers/NetworkManager.swift
@@ -18,68 +18,40 @@ final class NetworkManager {
     
     var holidays = [Holiday]()
     
-//    func getTodaysHoliday(completed: @escaping (Result<[Appetizer], TIError>) -> Void) {
-//        guard let url = URL(string: baseURL) else {
-//            completed(.failure(TIError.invalidURL))
-//            return
-//        }
-//
-//        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
-//            if let _ = error {
-//                completed(.failure(TIError.unableToComplete))
-//                return
-//            }
-//
-//            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-//                completed(.failure(TIError.invalidResponse))
-//                return
-//            }
-//
-//            guard let data = data else {
-//                completed(.failure(TIError.invalidData))
-//                return
-//            }
-//
-//            do {
-//                let decoder = try JSONDecoder().decode(AppetizerResponse.self, from: data)
-//                completed(.success(decoder.request))
-//            } catch {
-//                completed(.failure(TIError.invalidData))
-//            }
-//
-//        }
-//        task.resume()
-//    }
-    
     func findData() {
         let url = URL(string: baseURL)!
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { (data, response, error) in
-          guard let data = data else {
-            print("data was nil")
-            return
-          }
-          guard let htmlString = String(data: data, encoding: .utf8) else {
-            print("couldn't cast data into String")
-            return
-          }
-//            print(htmlString)
-           
+            guard let data = data else {
+                print("data was nil")
+                return
+            }
+            
+            guard let htmlString = String(data: data, encoding: .utf8) else {
+                print("couldn't cast data into String")
+                return
+            }
+            
             do {
-               
                 let doc: Document = try SwiftSoup.parse(htmlString)
                 let links: [Element] = try doc.select("h3 a").array()
                 
-                print("Holiday Name >>>>> \(try links.first?.text() ?? "No Holiday Name")")
-                print("Holiday link >>>>> \(try links.first?.attr("href") ?? "No Link")")
+                for title: Element in links {
+                    let linksText: String = try title.text()
+                    let linksHref: String = try title.attr("href")
+                    print(linksText, linksHref)
+                }
+                
+//                print("Holiday Name >>>>> \(try links.first?.text() ?? "No Holiday Name")")
+//                print("Holiday link >>>>> \(try links.first?.attr("href") ?? "No Link")")
             } catch Exception.Error(let type, let message) {
-                print(message)
+                print(type, message)
             } catch {
-                print("error")
+                print(error)
             }
             
         }
         task.resume()
-
+        
     }
     
 }

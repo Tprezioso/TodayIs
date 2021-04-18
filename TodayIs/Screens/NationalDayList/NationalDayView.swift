f1d2ac7bc197af6d4cdb5ff4b968319c4d17aa97
Thomas <tommyprezioso@gmail.com>
2021-04-18 10:12:47 -0400

    added some buttons and working on styling

--- TodayIs/Screens/NationalDayList/NationalDayView.swift
+++ TodayIs/Screens/NationalDayList/NationalDayView.swift
@@ -14,15 +14,22 @@ struct NationalDayView: View {
     
     var body: some View {
         VStack(spacing: 5) {
-            RemoteImage(image: viewModel.image)
-                .scaledToFit()
-                .padding(.bottom)
-            Text(viewModel.detailHoliday.description)
+            VStack {
+                RemoteImage(image: viewModel.image)
+                    .scaledToFit()
+                    .padding(.bottom)
+                Text(viewModel.detailHoliday.description)
+            }
             Spacer()
             Button {
-                
+                print("Share Button Tapped")
             } label: {
                 Text("Share")
+            }
+            Button {
+                print("Calendar button tapped")
+            } label: {
+                Text("Add to Calendar")
             }
             Spacer()
         }
@@ -38,48 +45,3 @@ struct NationalDayView: View {
         .padding()
     }
 }
-
-//class ImageLoader: ObservableObject {
-//    @Published var image: UIImage?
-//    private let url: URL
-//    private var cancellable: AnyCancellable?
-//
-//    init(url: URL) {
-//        self.url = url
-//    }
-//
-//    deinit {
-//        cancel()
-//    }
-//
-//    func load() {
-//        cancellable = URLSession.shared.dataTaskPublisher(for: url)
-//                    .map { UIImage(data: $0.data) }
-//                    .replaceError(with: nil)
-//                    .receive(on: DispatchQueue.main)
-//                    .sink { [weak self] in self?.image = $0 }
-//    }
-//
-//    func cancel() {
-//        cancellable?.cancel()
-//    }
-//}
-//
-//struct AsyncImage<Placeholder: View>: View {
-//    @StateObject private var loader: ImageLoader
-//    private let placeholder: Placeholder
-//
-//    init(url: URL, @ViewBuilder placeholder: () -> Placeholder) {
-//        self.placeholder = placeholder()
-//        _loader = StateObject(wrappedValue: ImageLoader(url: url))
-//    }
-//
-//    var body: some View {
-//        content
-//            .onAppear(perform: loader.load)
-//    }
-//
-//    private var content: some View {
-//        placeholder
-//    }
-//}

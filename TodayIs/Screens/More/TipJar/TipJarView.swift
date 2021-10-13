5353eed98e86b7bd1adb295143b45a4278d2795f
Thomas <tommyprezioso@gmail.com>
2021-10-13 09:07:38 -0400

    added images and support for ipados

--- TodayIs/Screens/More/TipJar/TipJarView.swift
+++ TodayIs/Screens/More/TipJar/TipJarView.swift
@@ -4,63 +4,63 @@
 //
 //  Created by Thomas Prezioso Jr on 6/2/21.
 //
-
-import SwiftUI
-import Purchases
-
-struct TipJarView: View {
-    @EnvironmentObject private var subscriptionManager: IAPManager
-
-        var body: some View {
-            VStack {
-                Text("Tip Jar")
-                    .font(.largeTitle)
-                    .padding(.horizontal)
-
-                VStack(alignment: .leading) {
-                    Text("If you love this app, you can leave a tip to cover development cost! Any tip at all helps a lot!")
-                        .fontWeight(.semibold)
-                }
-                .padding()
-
-                ScrollView {
-                    ForEach(subscriptionManager.packages, id: \.identifier) { product in
-                        Button(action: {
-                            subscriptionManager.purchase(product: product)
-                        }) {
-                            IAPRow(product: product)
-//                            APRow(title: "Coffee", description: "Buy me a coffee")
-                        }
-                    }
-                    .padding(.vertical)
-                }
-            }
-        }
-}
-
-struct TipJarView_Previews: PreviewProvider {
-    static var previews: some View {
-        TipJarView()
-    }
-}
-
-struct IAPRow: View {
-    var product: Purchases.Package
-
-    var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text(product.product.localizedTitle).bold()
-                Text(product.product.localizedDescription)
-            }
-
-            Spacer()
-
-            Text(product.localizedPriceString).bold()
-        }
-        .foregroundColor(.primary)
-        .padding(8)
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.horizontal)
-    }
-}
+//
+//import SwiftUI
+//import Purchases
+//
+//struct TipJarView: View {
+//    @EnvironmentObject private var subscriptionManager: IAPManager
+//
+//        var body: some View {
+//            VStack {
+//                Text("Tip Jar")
+//                    .font(.largeTitle)
+//                    .padding(.horizontal)
+//
+//                VStack(alignment: .leading) {
+//                    Text("If you love this app, you can leave a tip to cover development cost! Any tip at all helps a lot!")
+//                        .fontWeight(.semibold)
+//                }
+//                .padding()
+//
+//                ScrollView {
+//                    ForEach(subscriptionManager.packages, id: \.identifier) { product in
+//                        Button(action: {
+//                            subscriptionManager.purchase(product: product)
+//                        }) {
+//                            IAPRow(product: product)
+////                            APRow(title: "Coffee", description: "Buy me a coffee")
+//                        }
+//                    }
+//                    .padding(.vertical)
+//                }
+//            }
+//        }
+//}
+//
+//struct TipJarView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TipJarView()
+//    }
+//}
+//
+//struct IAPRow: View {
+//    var product: Purchases.Package
+//
+//    var body: some View {
+//        HStack {
+//            VStack(alignment: .leading) {
+//                Text(product.product.localizedTitle).bold()
+//                Text(product.product.localizedDescription)
+//            }
+//
+//            Spacer()
+//
+//            Text(product.localizedPriceString).bold()
+//        }
+//        .foregroundColor(.primary)
+//        .padding(8)
+//        .frame(maxWidth: .infinity, alignment: .leading)
+//        .padding(.horizontal)
+//    }
+//}
--- TodayIs/Utilities/Managers/IAPManager.swift
+++ TodayIs/Utilities/Managers/IAPManager.swift
@@ -5,30 +5,30 @@
 //  Created by Thomas Prezioso Jr on 6/2/21.
 //
 
-import Purchases
-import SwiftUI
-
-class IAPManager: ObservableObject {
-    static let shared = IAPManager()
-
-    @Published var packages: [Purchases.Package] = []
-    @Published var inPaymentProgress = false
-
-    init() {
-        Purchases.debugLogsEnabled = true
-        Purchases.configure(withAPIKey: "MUQJwNwqVNadGbiooHEzVsojeHldTzyL")
-        Purchases.shared.offerings { (offerings, _) in
-            if let packages = offerings?.current?.availablePackages {
-                self.packages = packages
-            }
-        }
-    }
-
-    func purchase(product: Purchases.Package) {
-        guard !inPaymentProgress else { return }
-        inPaymentProgress = true
-        Purchases.shared.purchasePackage(product) { (_, purchaserInfo, _, _) in
-            self.inPaymentProgress = false
-        }
-    }
-}
+//import Purchases
+//import SwiftUI
+//
+//class IAPManager: ObservableObject {
+//    static let shared = IAPManager()
+//
+//    @Published var packages: [Purchases.Package] = []
+//    @Published var inPaymentProgress = false
+//
+//    init() {
+//        Purchases.debugLogsEnabled = true
+//        Purchases.configure(withAPIKey: "MUQJwNwqVNadGbiooHEzVsojeHldTzyL")
+//        Purchases.shared.offerings { (offerings, _) in
+//            if let packages = offerings?.current?.availablePackages {
+//                self.packages = packages
+//            }
+//        }
+//    }
+//
+//    func purchase(product: Purchases.Package) {
+//        guard !inPaymentProgress else { return }
+//        inPaymentProgress = true
+//        Purchases.shared.purchasePackage(product) { (_, purchaserInfo, _, _) in
+//            self.inPaymentProgress = false
+//        }
+//    }
+//}
